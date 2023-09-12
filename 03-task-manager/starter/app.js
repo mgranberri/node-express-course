6ac3ee6ebba298cf64c1fb6723b9a433262b0e98
Mario Granberri <mariogranberri@Marios-MBP.lan>
2023-09-11 17:08:24 -0700

    node task manager final

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,29 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDb = require('./db/connect');
+require('dotenv').config();
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+//Middleware
+app.use(express.static('./public')) 
+app.use(express.json())
+
+// Routes
+app.use('/api/v1/tasks', tasks)
+
+app.use(notFound)
+app.use(errorHandlerMiddleware)
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        await connectDb(process.env.MONGO_URI);
+        app.listen(port, console.log(`Server is listening on port ${port}...` ));
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start()
